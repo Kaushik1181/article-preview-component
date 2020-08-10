06622bb9235e95b5a2179fd12af4826e45575354
Kaushik <47687763+Kaushik1181@users.noreply.github.com>
2020-08-10 09:06:45 +0530

    master

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover({
     trigger: 'focus',
-    placement : 'top',
+    placement : 'left',
 	  html : true,
     content : '<div class="social-icons bg-dark text-white rounded"><h5 class="icons">SHARE</h5><i class="fab fa-twitter icons"></i><i class="fab fa-facebook icons"></i><i class="fab fa-pinterest icons"></i></div>',
     });
