a9dd6eafea5f11e69570de5af5e9a9366250176f
Kaushik <47687763+Kaushik1181@users.noreply.github.com>
2020-08-10 08:28:18 +0530

    commit viewport

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover({
-    trigger: 'click',
+    trigger: 'focus',
     placement : 'top',
 	  html : true,
     content : '<div class="social-icons bg-dark text-white rounded"><h4 class="icons">SHARE</h4><i class="fab fa-twitter fa-2x icons"></i><i class="fab fa-facebook fa-2x icons"></i><i class="fab fa-pinterest fa-2x icons"></i></div>',
